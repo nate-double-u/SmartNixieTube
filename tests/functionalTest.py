de06474cf82fe77579d36af6d78dcf6eea35efeb
Nathan Waddington <Nathan.Waddington@akqa.com>
2015-10-28 13:43:03 -0700

    Adding License Cleaning up Functional test for example updating git ignore removing docs folder as it should all be available at http://switchmodedesign.com/products/smart-nixie-tube

--- tests/functionalTest.py
+++ tests/functionalTest.py
@@ -1,31 +1,36 @@
+import random
+
 __author__ = 'Nathan Waddington'
 __email__ = 'nathan_waddington@alumni.sfu.ca'
-
-import random
 
 try:
     from smartnixietube.SmartNixieTube import SmartNixieTubeDisplay
 except ImportError as e:
     import sys
     import os
+
     PACKAGE_PARENT = '..'
     SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
     sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
     from smartnixietube.SmartNixieTube import SmartNixieTubeDisplay
 
+# set number of tubes in the display
 numberOfTubesInDisplay = 3
+
+# instantiate the display -- serial ports tested on Linux and Mac OS X.
 display = SmartNixieTubeDisplay(numberOfTubesInDisplay, '/dev/tty.usbserial-A9QHHRFJ')  # '/dev/tty.usbserial-A9UD9RRV')
 
+# all the properties can be set like this:
 display.brightness = 255
 
 for i in range(255):
-    display.red = random.randint(0,255)
-    display.green = random.randint(0,255)
-    display.blue = random.randint(0,255)
+    display.red = random.randint(0, 255)
+    display.green = random.randint(0, 255)
+    display.blue = random.randint(0, 255)
 
+    # set the display number
     display.setDisplayNumber(i)  # random.randint(0,999))
 
-    # print(i, display.generateCommandString().encode())
-
+    # send the command set to the display
     display.sendCommand()
