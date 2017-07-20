11c3b46ead32dfc597995e804a6dc6862a96286a
Nathan Waddington <Nathan.Waddington@akqa.com>
2017-07-20 11:14:19 -0700

    changing the timing of the functional test - colours change as fast as possible, number changes every second.

--- tests/functionalTest.py
+++ tests/functionalTest.py
@@ -1,4 +1,5 @@
 import random
+import threading
 
 __author__ = 'Nathan Waddington'
 __email__ = 'nathan_waddington@alumni.sfu.ca'
@@ -19,18 +20,28 @@ except ImportError as e:
 numberOfTubesInDisplay = 3
 
 # instantiate the display -- serial ports tested on Linux and Mac OS X.
-display = SmartNixieTubeDisplay(numberOfTubesInDisplay, '/dev/tty.usbserial-A9QHHRFJ')  # '/dev/tty.usbserial-A9UD9RRV')
+display = SmartNixieTubeDisplay(numberOfTubesInDisplay, '/dev/ttyUSB0')  # '/dev/tty.usbserial-A9UD9RRV')
 
 # all the properties can be set like this:
 display.brightness = 255
 
+
+# set random number every second
+def set_number():
+    global display
+    threading.Timer(1.0, set_number).start()
+    display.set_display_number(random.randint(0, 999))
+
+set_number()
+
+# do the thing
 for i in range(255):
     display.red = random.randint(0, 255)
     display.green = random.randint(0, 255)
     display.blue = random.randint(0, 255)
 
     # set the display number
-    display.set_display_number(i)  # random.randint(0,999))
+    # display.set_display_number(i)  # random.randint(0,999))
 
     # send the command set to the display
     display.send_command()
